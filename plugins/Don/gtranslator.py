40386e9b8bc3798a21a7b829b134bca538083dad
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-01-29 19:55:18 +0530

    Update gtranslator.py

--- plugins/Don/gtranslator.py
+++ plugins/Don/gtranslator.py
@@ -2,7 +2,7 @@ from googletrans import Translator
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from plugins.admemes.list import list
-from database.gtrans_mdb import find_one
+import find_one
 
 @Client.on_message(filters.command(["tr"]))
 async def left(client,message):
