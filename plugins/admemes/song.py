e7a52f39c6ae23bdd42d221a0fbad534fa251bd8
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-01-30 14:05:23 +0530

    Update song.py

--- plugins/admemes/song.py
+++ plugins/admemes/song.py
@@ -131,7 +131,9 @@ async def progress(current, total, message, start, type_of_ps, file_name=None):
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
         progress_str = "{0}{1} {2}%\n".format(
-            "".join("֍" for i in range(math.floor(percentage / 10))),
+
+            "".join("▓" for i in range(math.floor(percentage / 10))),
+
             "".join("ｏ" for i in range(10 - math.floor(percentage / 10))),
             round(percentage, 2),
         )
