ddbf48a1bbd3d141d41529ebc2332a9de3c90054
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-01-31 14:02:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -717,7 +717,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('💠 GᏒԾUᎮ 💠', url='https://t.me/+EqhXLhL3T1w4Zjc1'),
             InlineKeyboardButton('💠 CHAИИΞL 💠', url='https://t.me/+veUIdIW2CQ5mOGU5')
             ],[
-            InlineKeyboardButton('💰 𝗗𝗢𝗡𝗔𝗧𝗘 〄 𝗣𝗥𝗢𝗠𝗢𝗧𝗜𝗢𝗡 💰', callback_data='donation')
+            InlineKeyboardButton('💰 𝗗𝗢𝗡𝗔𝗧𝗘 〄 𝗣𝗔𝗜𝗗 𝗣𝗥𝗢𝗠𝗢𝗧𝗜𝗢𝗡 💰', callback_data='donation')
             ],[
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
