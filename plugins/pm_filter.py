89760f87a9ef62fe8bcde45ae9498664fd85af26
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-02-01 16:45:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1014,7 +1014,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.PROMOTION_TXT,
+            text=script.FILESTORE_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
