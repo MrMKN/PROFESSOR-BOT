bd4bac60d5afa6f35b7aea3fc382a066a757fa07
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-02-25 17:59:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -831,13 +831,22 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text="▣▢▢"
+            text="▣▢▢▢▢▢"
         )
         await query.message.edit_text(
-            text="▣▣▢"
+            text="▣▣▢▢▢▢"
         )
         await query.message.edit_text(
-            text="▣▣▣"
+            text="▣▣▣▢▢▢"
+        )
+        await query.message.edit_text(
+            text="▣▣▣▣▢▢"
+        )
+        await query.message.edit_text(
+            text="▣▣▣▣▣▢"
+        )
+        await query.message.edit_text(
+            text="▣▣▣▣▣▣"
         )
         await query.message.edit_text(
             text=script.ABOUT_TXT,
