b23f998c44d0f4076bfb68453d1ae0845354594a
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-02-25 20:10:48 +0530

    അടിച്ചുപോയി

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -822,7 +822,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             disable_web_page_preview=True,
             reply_markup=reply_markup,
             parse_mode='html'
-        )parse_mode='html
+        )
     elif query.data == "image":
         buttons= [[
             InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='help')
