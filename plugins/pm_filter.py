122dd055ff4373ceecbb290dc66b1999eb37a32e
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-02-25 19:31:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -772,109 +772,31 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text="𝙾"
         )
         await query.message.edit_text(
-            text="𝙿"
+            text="𝙾𝙿"
         )
         await query.message.edit_text(
-            text="𝚄"
+            text="𝙾𝙿𝚄"
         )
         await query.message.edit_text(
-            text="𝚂"
+            text="𝙾𝙿𝚄𝚂"
         )
         await query.message.edit_text(
-            text="ㅤ"
+            text="𝙾𝙿𝚄𝚂-"
         )
         await query.message.edit_text(
-            text="𝚃"
+            text="𝙾𝙿𝚄𝚂-𝚃"
         )
         await query.message.edit_text(
-            text="𝙴"
+            text="𝙾𝙿𝚄𝚂-𝚃𝙴"
         )
         await query.message.edit_text(
-            text="𝙲"
+            text="𝙾𝙿𝚄𝚂-𝚃𝙴𝙲"
         )
         await query.message.edit_text(
-            text="𝙷"
+            text="𝙾𝙿𝚄𝚂-𝚃𝙴𝙲𝙷"
         )
         await query.message.edit_text(
-            text="𝚉"
-        )
-        await query.message.edit_text(
-            text=script.HELP_TXT.format(query.from_user.mention),
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
-    elif query.data == "hellp":
-        buttons = [[
-            InlineKeyboardButton('𝙼𝙰𝙽𝚄𝙴𝙻 𝙵𝙸𝙻𝚃𝙴𝚁', callback_data='manuelfilter'),
-            InlineKeyboardButton('𝙰𝚄𝚃𝙾 𝙵𝙸𝙻𝚃𝙴𝚁', callback_data='autofilter'),
-            InlineKeyboardButton('𝙲𝙾𝙽𝙽𝙴𝙲𝚃𝙸𝙾𝙽𝚂', callback_data='coct')
-            ],[
-            InlineKeyboardButton('𝚂𝙾𝙽𝙶', callback_data='songs'),
-            InlineKeyboardButton('𝙴𝚇𝚃𝚁𝙰', callback_data='extra'),
-            InlineKeyboardButton("𝚅𝙸𝙳𝙴𝙾", callback_data='video')
-            ],[
-            InlineKeyboardButton('𝙿𝙸𝙽', callback_data='pin'), 
-            InlineKeyboardButton('𝙿𝙰𝚂𝚃𝙴', callback_data='pastes'),
-            InlineKeyboardButton("𝙸𝙼𝙰𝙶𝙴", callback_data='image')
-            ],[
-            InlineKeyboardButton('𝙵𝚄𝙽', callback_data='fun'), 
-            InlineKeyboardButton('𝙹𝚂𝙾𝙽𝙴', callback_data='son'),
-            InlineKeyboardButton('𝚃𝚃𝚂', callback_data='ttss')
-            ],[
-            InlineKeyboardButton('𝙿𝚄𝚁𝙶𝙴', callback_data='purges'),
-            InlineKeyboardButton('𝙿𝙸𝙽𝙶', callback_data='pings'),
-            InlineKeyboardButton('𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙿𝙷', callback_data='tele')
-            ],[
-            InlineKeyboardButton('𝚆𝙷𝙾𝙸𝚂', callback_data='whois'),
-            InlineKeyboardButton('𝙼𝚄𝚃𝙴', callback_data='restric'),
-            InlineKeyboardButton('𝙺𝙸𝙲𝙺', callback_data='zombies')
-            ],[
-            InlineKeyboardButton('𝚁𝙴𝙿𝙾𝚁𝚃', callback_data='report'),
-            InlineKeyboardButton('𝚈𝚃-𝚃𝙷𝚄𝙼𝙱', callback_data='ytthumb'),
-            InlineKeyboardButton('𝚂𝚃𝙸𝙲𝙺𝙴𝚁-𝙸𝙳', callback_data='sticker')
-            ],[
-            InlineKeyboardButton('𝙲𝙾𝚅𝙸𝙳', callback_data='corona'),
-            InlineKeyboardButton('𝙰𝚄𝙳𝙸𝙾-𝙱𝙾𝙾𝙺', callback_data='abook'),
-            InlineKeyboardButton('𝚄𝚁𝙻-𝚂𝙷𝙾𝚁𝚃', callback_data='urlshort')
-            ],[
-            InlineKeyboardButton('𝙶-𝚃𝚁𝙰𝙽𝚂', callback_data='gtrans'),
-            InlineKeyboardButton('𝙵𝙸𝙻𝙴-𝚂𝚃𝙾𝚁𝙴', callback_data='newdata'),
-            InlineKeyboardButton('𝚂𝚃𝙰𝚃𝚄𝚂', callback_data='stats')
-            ],[
-            InlineKeyboardButton('𝙷𝙾𝚆 𝚃𝙾 𝙳𝙴𝙿𝙻𝙾𝚈..?', callback_data='deploy')
-            ],[
-            InlineKeyboardButton('⚚ 𝙱𝙰𝙲𝙺 ⚚', callback_data='start')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text="𝙾"
-        )
-        await query.message.edit_text(
-            text="𝙿"
-        )
-        await query.message.edit_text(
-            text="𝚄"
-        )
-        await query.message.edit_text(
-            text="𝚂"
-        )
-        await query.message.edit_text(
-            text="ㅤ"
-        )
-        await query.message.edit_text(
-            text="𝚃"
-        )
-        await query.message.edit_text(
-            text="𝙴"
-        )
-        await query.message.edit_text(
-            text="𝙲"
-        )
-        await query.message.edit_text(
-            text="𝙷"
-        )
-        await query.message.edit_text(
-            text="𝚉"
+            text="𝙾𝙿𝚄𝚂-𝚃𝙴𝙲𝙷𝚉"
         )
         await query.message.edit_text(
             text=script.ABOUT_TXT,
