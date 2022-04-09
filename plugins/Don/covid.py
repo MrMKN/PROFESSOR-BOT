1a82d623c097f74d227454019ef12c96b4d731c1
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-04-09 15:05:10 +0530

    Update covid.py

--- plugins/Don/covid.py
+++ plugins/Don/covid.py
@@ -12,7 +12,7 @@ BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton("𝙲𝙻𝙾𝚂𝙴", ca
 async def reply_info(client, message):
     query = message.text.split(None, 1)[1]
     await message.reply_photo(
-        photo="https://telegra.ph/file/1b837a8df2670b0097aaf.jpg",
+        photo="https://telegra.ph/file/2e2a07e86066538ed7406.jpg",
         caption=covid_info(query),
         quote=True
     )
