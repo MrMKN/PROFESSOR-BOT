d9fe3e2fb21c218ec64e677a7e614982c7878fec
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-04-09 11:16:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/MrMKN/Ajax-Extra-Features.git /Ajax
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Ajax
 fi
 cd /Ajax
 pip3 install -U -r requirements.txt
-echo "Starting ᗩᒍᗩ᙭....🔥"
+echo "Starting 𝙼𝙺𝙽 𝙱𝙾𝚃𝚉....🔥"
 python3 bot.py
