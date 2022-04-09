0d64ce1e2031242641733277cd6beb3f17dee0c1
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-04-09 13:49:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -830,7 +830,16 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)       
         await query.message.edit_text(
-            text="MKN BOTZ"
+            text="◻️◼️◼️◼️"
+        )
+        await query.message.edit_text(
+            text="◻️◻️◼️◼️"
+        )
+        await query.message.edit_text(
+            text="◻️◻️◻️◼️"
+        )
+        await query.message.edit_text(
+            text="◻️◻️◻️◻️"
         )
         await query.message.edit_text(                     
             text=script.HELP_TXT.format(query.from_user.mention),
@@ -844,13 +853,16 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text="◻️◼️◼️"
+            text="◻️◼️◼️◼️"
         )
         await query.message.edit_text(
-            text="◻️◻️◼️"
+            text="◻️◻️◼️◼️"
         )
         await query.message.edit_text(
-            text="◻️◻️◻️"
+            text="◻️◻️◻️◼️"
+        )
+        await query.message.edit_text(
+            text="◻️◻️◻️◻️"
         )
         await query.message.edit_text(
             text=script.ABOUT_TXT,
