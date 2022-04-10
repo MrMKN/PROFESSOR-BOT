064e9f191a42f2c8be97350714fb7088b501eba6
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-04-10 09:34:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MrMKN/Ajax-Extra-Features.git /Ajax
+  git clone https://github.com/MrMKN/Professor-Bot.git /Ajax
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Ajax
