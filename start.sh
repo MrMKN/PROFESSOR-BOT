2fafd403ef084e6d8e0d3787e9383aa35f44cba8
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-04-13 12:19:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MrMKN/Professor-Bot.git /Ajax
+  git clone https://github.com/surabhichinnu1/Professor-Bot.git /Professor-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /Professor-Bot
 fi
 cd /Ajax
 pip3 install -U -r requirements.txt
-echo "Starting 𝙼𝙺𝙽 𝙱𝙾𝚃𝚉....🔥"
+echo "Starting 𝙿𝚛𝚘𝚏𝚎𝚜𝚜𝚘𝚛 𝙱𝙾𝚃....🔥"
 python3 bot.py
