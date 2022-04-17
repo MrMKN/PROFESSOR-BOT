90658c612d6e81b3b4a311792ff0864c944c4ad9
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-04-17 08:38:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -464,7 +464,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 f_caption = f_caption
             if f_caption is None:
                 f_caption = f"{files.file_name}"
-
+                
             try:
                 if AUTH_CHANNEL and not await is_subscribed(client, query):
                     await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
