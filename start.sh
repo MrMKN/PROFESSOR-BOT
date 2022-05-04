4edcd17d51f7a39f7fdd8656205fdfbd802769b8
Thorappan-TG <103317425+Thorappan-TG@users.noreply.github.com>
2022-05-04 23:05:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MrMKN/Professor-Bot.git /Professor-Bot
+  git clone https://github.com/Thorappan-TG/Professor-Bot.git /Professor-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Professor-Bot
