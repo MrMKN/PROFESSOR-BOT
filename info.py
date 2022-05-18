e8cc0caaadad538ec94391706a433a24aa29d884
Lolan <102511853+Jeolpaul@users.noreply.github.com>
2022-05-18 17:11:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,6 +96,10 @@ MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
+DELETE_TIME = int(environ.get('DELETE_TIME', 300))
+CH_LINK = environ.get('https://t.me/Mallumovies099', "")
+CH_FILTER = int(environ.get('-1001688240145', 0))
+
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
 LOG_STR += ("P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n")
