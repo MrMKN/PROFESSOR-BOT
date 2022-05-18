5a1c5afbdbce9d82f9854afd0be221b9bfa2ef78
Lolan <102511853+Jeolpaul@users.noreply.github.com>
2022-05-18 11:03:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('📢 UPDATES 📢', url='https://t.me/mkn_bots_updates')
+                InlineKeyboardButton('📢 UPDATES 📢', url='https://t.me/BETA_UPDATES')
             ],
             [
                 InlineKeyboardButton('ℹ️ Help ℹ️', url=f"https://t.me/{temp.U_NAME}?start=help")
@@ -45,8 +45,8 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕️ Add Me To Your Group ➕️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🤠 Dev 🤠', url='https://t.me/mr_MKN'), 
-            InlineKeyboardButton('📢 UPDATES 📢', url='https://t.me/mkn_bots_updates')
+            InlineKeyboardButton('࿏ OWNER ࿏', url='https://t.me/JP_Jeol'), 
+            InlineKeyboardButton('📢 UPDATES 📢', url='https://t.me/BETA_UPDATES')
             ],[      
             InlineKeyboardButton('ℹ️ Help ℹ️', callback_data='help'),
             InlineKeyboardButton('😊 About 😊', callback_data='about')
@@ -90,8 +90,8 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕️ Add Me To Your Group ➕️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🤠 Dev 🤠', url='https://t.me/mr_MKN'), 
-            InlineKeyboardButton('📢 UPDATES 📢', url='https://t.me/mkn_bots_updates')
+            InlineKeyboardButton('࿏ OWNER ࿏', url='https://t.me/JP_Jeol'), 
+            InlineKeyboardButton('📢 UPDATES 📢', url='https://t.me/BETA_UPDATES')
             ],[      
             InlineKeyboardButton('ℹ️ Help ℹ️', callback_data='help'),
             InlineKeyboardButton('😊 About 😊', callback_data='about')
