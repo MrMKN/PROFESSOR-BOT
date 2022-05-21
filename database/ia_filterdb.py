ed98109ba80fb7724ae1ab3ab4efac3c58b7d397
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-21 16:31:02 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -62,7 +62,7 @@ async def save_file(media):
 
 
 
-async def get_search_results(query, file_type=None, max_results=10, offset=0, filter=False):
+async def get_search_results(query, file_type=None, max_results=6, offset=0, filter=False):
     """For given query return (results, next_offset)"""
 
     query = query.strip()
