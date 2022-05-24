1f3e2b9e1ecc04ceb2c774b2668f75f4418b058d
Lolan <102511853+Jeolpaul@users.noreply.github.com>
2022-05-24 19:10:07 +0530

    Update song.py

--- plugins/admemes/song.py
+++ plugins/admemes/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**BY›› [𝘽𝙀𝙏𝘼 Bᴏᴛᴢ™](https://t.me/BETA_UPDATES)**'
+        rep = '**BY›› [𝘽𝙀𝙏𝘼 Bᴏᴛᴢ](https://t.me/beta_bot_updates)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
