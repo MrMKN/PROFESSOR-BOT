3a500cccd1a973605d1941b671ab40d29c01e617
Lolan <102511853+Jeolpaul@users.noreply.github.com>
2022-06-13 09:59:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -494,7 +494,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data.startswith("checksub"):
         if AUTH_CHANNEL and not await is_subscribed(client, query):
-            await query.answer("I Like Your Smartness, But Don't Be Oversmart Okay", show_alert=True)
+            await query.answer("I Know You're Smart, But Don't Be Oversmart", show_alert=True)
             return
         ident, file_id = query.data.split("#")
         files_ = await get_file_details(file_id)
@@ -864,13 +864,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)       
         await query.message.edit_text(
-            text="⦿"
+            text="❃"
         )
         await query.message.edit_text(
-            text="⦿⦿⦿"
+            text="✰✰✰"
         )
         await query.message.edit_text(
-            text="⦿⦿⦿⦿⦿⦿⦿"
+            text="❃✰✺❃✰✺"
         )
         await query.message.edit_text(                     
             text=script.HELP_TXT.format(query.from_user.mention),
@@ -886,13 +886,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text="⦿"
+            text="❃"
         )
         await query.message.edit_text(
-            text="⦿⦿⦿"
+            text="✰✰✰"
         )
         await query.message.edit_text(
-            text="⦿⦿⦿⦿⦿⦿⦿"
+            text="❃✰✺❃✰✺"
         ) 
         await query.message.edit_text(
             text=script.ABOUT_TXT.format(temp.B_NAME),
