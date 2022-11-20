ae5caba5e7c97b615249d668dfd14773c7a56270
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-11-21 01:08:26 +0530

    professor v3

---
+++ plugins/helper/extract.py
@@ -0,0 +1,63 @@
+from pyrogram.types import Message
+import time
+
+def extract_user(message: Message) -> (int, str):
+    """extracts the user from a message"""
+    user_id = None
+    user_first_name = None
+
+    if message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        user_first_name = message.reply_to_message.from_user.first_name             
+
+    elif len(message.command) > 1:
+        if (
+            len(message.entities) > 1 and
+            message.entities[1].type == "text_mention"
+        ):
+            # 0: is the command used
+            # 1: should be the user specified
+            required_entity = message.entities[1]
+            user_id = required_entity.user.id
+            user_first_name = required_entity.user.first_name
+        else:
+            user_id = message.command[1]
+            # don't want to make a request -_-
+            user_first_name = user_id
+
+        try:
+            user_id = int(user_id)
+        except ValueError:
+            print("unknown")
+
+    else:
+        user_id = message.from_user.id
+        user_first_name = message.from_user.first_name
+
+    return (user_id, user_first_name)
+
+
+def extract_time(time_val):
+    if any(time_val.endswith(unit) for unit in ('s', 'm', 'h', 'd')):
+        unit = time_val[-1]
+        time_num = time_val[:-1]  # type: str
+        if not time_num.isdigit():
+            return None
+
+        if unit == 's':
+            bantime = int(time.time() + int(time_num))
+        elif unit == 'm':
+            bantime = int(time.time() + int(time_num) * 60)
+        elif unit == 'h':
+            bantime = int(time.time() + int(time_num) * 60 * 60)
+        elif unit == 'd':
+            bantime = int(time.time() + int(time_num) * 24 * 60 * 60)
+        else:
+            # how even...?
+            return None
+        return bantime
+    else:
+        return None
+
+
+
