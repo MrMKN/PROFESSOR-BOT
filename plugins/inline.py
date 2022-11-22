8e9833125785e7567bfc342d51e44147de8680d1
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-22 23:13:48 +0530

    Fix

--- plugins/inline.py
+++ plugins/inline.py
@@ -59,7 +59,7 @@ async def answer(bot, query):
         f_caption=file.caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
+                f_caption=CUSTOM_FILE_CAPTION.format(mention=query.from_user.mention, file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
             except Exception as e:
                 logger.exception(e)
                 f_caption=f_caption
