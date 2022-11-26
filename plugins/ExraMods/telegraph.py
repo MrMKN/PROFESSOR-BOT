5d56229463d06471d00616efe1031ccc143c8729
Athul <103881131+athulx80@users.noreply.github.com>
2022-11-26 15:47:29 +0530

    Update telegraph.py

--- plugins/ExraMods/telegraph.py
+++ plugins/ExraMods/telegraph.py
@@ -10,7 +10,7 @@ from utils import get_file_id
 async def telegraph_upload(bot, update):
     replied = update.reply_to_message
     if not replied:
-        await update.reply_text("𝚁𝙴𝙿𝙻𝚈 𝚃𝙾 𝙰 𝙿𝙷𝙾𝚃𝙾 𝙾𝚁 𝚅𝙸𝙳𝙴𝙾 𝚄𝙽𝙳𝙴𝚁 𝟻𝙼𝙱.")
+        await update.reply_text("𝖱𝖾𝗉𝗅𝗒 𝖳𝗈 𝖺 𝗉𝗁𝗈𝗍𝗈 𝗈𝗋 𝖵𝗂𝖽𝖾𝗈 𝗎𝗇𝖽𝖾𝗋 𝟧𝖬𝖡.")
         return
     file_info = get_file_id(replied)
     if not file_info:
@@ -37,7 +37,7 @@ async def telegraph_upload(bot, update):
             InlineKeyboardButton(text="Open Link", url=f"https://graph.org{response[0]}"),
             InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://graph.org{response[0]}")
             ],[
-            InlineKeyboardButton(text="✗ Close ✗", callback_data="close")
+            InlineKeyboardButton(text="🔒 ᴄʟᴏꜱᴇ", callback_data="close")
             ]])
         )
     
