94361a07ea64fa4b676f34c1abc84121fd07a43f
Athul <103881131+athulx80@users.noreply.github.com>
2022-11-26 15:53:26 +0530

    Update share_text.py

--- plugins/ExraMods/share_text.py
+++ plugins/ExraMods/share_text.py
@@ -14,13 +14,13 @@ async def share_text(client, message):
         input_text = reply.text or reply.caption
     else:
         await message.reply_text(
-            text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",                
+            text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**If you have any question send message.**",                
             reply_to_message_id=reply_id,               
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Support Chat", url=f"https://t.me/MKN_BOTZ_DISCUSSION_GROUP")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴄʟɪᴄᴋ ʜᴇʀᴇ", url=f"https://t.me/MEmpire_Comment_Bot")]])
             )                                                   
         return
     await message.reply_text(
-        text=f"**Here is Your Sharing Text 👇**\n\nhttps://t.me/share/url?url=" + quote(input_text),
+        text=f"**Here is Your Sharing Text ⬇️**\n\nhttps://t.me/share/url?url=" + quote(input_text),
         reply_to_message_id=reply_id,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("♂️ Share", url=f"https://t.me/share/url?url={quote(input_text)}")]])       
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("♂️ ꜱʜᴀʀᴇ", url=f"https://t.me/share/url?url={quote(input_text)}")]])       
     )
