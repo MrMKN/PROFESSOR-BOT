6ef02a84fb86b7547461ad9abc63069aba7adb60
Athul <103881131+athulx80@users.noreply.github.com>
2022-11-26 14:25:37 +0530

    Update photo.py

--- plugins/photo.py
+++ plugins/photo.py
@@ -38,7 +38,7 @@ async def photo(client: Client, message: Message):
                         ),
                     ],
                     [
-                        InlineKeyboardButton(text="𝖢𝗅𝗈𝗌𝖾", callback_data="close_data"),
+                        InlineKeyboardButton(text="🔒 ᴄʟᴏꜱᴇ", callback_data="close_data"),
                     ],
                 ]
             ),
