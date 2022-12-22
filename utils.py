a944b6d1cf271a58869d26e55ee28cc4ad1c329b
Mhdrzn <103956396+Mhdrzn@users.noreply.github.com>
2022-12-22 10:34:59 +0530

    Fix imdb

--- utils.py
+++ utils.py
@@ -69,7 +69,10 @@ async def get_poster(query, bulk=False, id=False, file=None):
                 year = list_to_str(year[:1]) 
         else:
             year = None
-        movieid = imdb.search_movie(title.lower(), results=10)
+        try:
+           movieid = imdb.search_movie(title.lower(), results=10)
+        except:
+           return None
         if not movieid:
             return None
         if year:
