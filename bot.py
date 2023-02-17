f738ac59e1ea1ded2ecd919648be9118140adbb5
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-02-17 11:50:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
-import logging, os, math
-import logging.config
+import os, math
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
@@ -14,13 +13,8 @@ from pyrogram.errors import BadRequest, Unauthorized
 from plugins import web_server
 from aiohttp import web
 
-# Get logging configurations
-logging.config.fileConfig("logging.conf")
-logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("cinemagoer").setLevel(logging.ERROR)
-LOGGER = logging.getLogger(__name__)
 TIMEZONE = (os.environ.get("TIMEZONE", "Asia/Kolkata"))
+
 class Bot(Client):
 
     def __init__(self):
@@ -53,20 +47,20 @@ class Bot(Client):
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
-        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-        logging.info(LOG_STR)
+        print(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        print(LOG_STR)
         if LOG_CHANNEL:
             try:
                 await self.send_message(LOG_CHANNEL, text=f"<b>{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!\n\n📅 Dᴀᴛᴇ : <code>{date}</code>\n⏰ Tɪᴍᴇ : <code>{time}</code>\n🌐 Tɪᴍᴇᴢᴏɴᴇ : <code>{TIMEZONE}</code>\n\n🉐 Vᴇʀsɪᴏɴ : <code>v{__version__} (Layer {layer})</code></b>")                      
             except Unauthorized:
-                LOGGER.warning("Bot isn't able to send message to LOG_CHANNEL")
+                print("Bot isn't able to send message to LOG_CHANNEL")
             except BadRequest as e:
-                LOGGER.error(e)                         
+                print(e)                         
 
     async def stop(self, *args):
         await super().stop()
         me = await self.get_me()
-        logging.info(f"{me.first_name} is_...  ♻️Restarting...")
+        print(f"{me.first_name} is_...  ♻️Restarting...")
 
     async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:                       
         current = offset
