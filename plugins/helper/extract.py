aa5ca9b5a0dc77424f1900ce839d7c655aa97cfb
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-02-18 13:06:57 +0530

    `'Int' object has not attribute 'timestamp'` fixed

--- plugins/helper/extract.py
+++ plugins/helper/extract.py
@@ -1,5 +1,6 @@
 from pyrogram.types import Message
 import time
+from datetime import datetime, timedelta
 
 def extract_user(message: Message) -> (int, str):
     """extracts the user from a message"""
@@ -36,28 +37,25 @@ def extract_user(message: Message) -> (int, str):
 
     return (user_id, user_first_name)
 
-
+# from Midukki-RoBoT
 def extract_time(time_val):
-    if any(time_val.endswith(unit) for unit in ('s', 'm', 'h', 'd')):
+    if any(time_val.endswith(unit) for unit in ("s", "m", "h", "d")):
         unit = time_val[-1]
         time_num = time_val[:-1]  # type: str
         if not time_num.isdigit():
             return None
 
-        if unit == 's':
-            bantime = int(time.time() + int(time_num))
-        elif unit == 'm':
-            bantime = int(time.time() + int(time_num) * 60)
-        elif unit == 'h':
-            bantime = int(time.time() + int(time_num) * 60 * 60)
-        elif unit == 'd':
-            bantime = int(time.time() + int(time_num) * 24 * 60 * 60)
+        if unit == "s":
+            bantime = datetime.now() + timedelta(seconds=int(time_num)) 
+        elif unit == "m":
+            bantime = datetime.now() + timedelta(minutes=int(time_num))
+        elif unit == "h":
+            bantime = datetime.now() + timedelta(hours=int(time_num))
+        elif unit == "d":
+            bantime = datetime.now() + timedelta(days=int(time_num))
         else:
             # how even...?
             return None
         return bantime
     else:
         return None
-
-
-
