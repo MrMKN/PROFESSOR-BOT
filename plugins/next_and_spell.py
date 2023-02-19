ef4514e92d6d5372cf9f2d4aef23825084a2dea5
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-02-19 12:17:33 +0530

    Update next_and_spell.py

--- plugins/next_and_spell.py
+++ plugins/next_and_spell.py
@@ -46,13 +46,12 @@ async def next_page(bot, query):
         return
     settings = await get_settings(query.message.chat.id)
     nxreq  = query.from_user.id if query.from_user else 0
-    if SHORT_URL and SHORT_API:
-        url_link = await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")          
+    if SHORT_URL and SHORT_API:          
         if settings["button"]:
-            btn = [[InlineKeyboardButton(text=f"[{get_size(file.file_size)}] {file.file_name}", url=url_link)] for file in files ]
+            btn = [[InlineKeyboardButton(text=f"[{get_size(file.file_size)}] {file.file_name}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}"))] for file in files ]
         else:
-            btn = [[InlineKeyboardButton(text=f"{file.file_name}", url=url_link),
-                    InlineKeyboardButton(text=f"{get_size(file.file_size)}", url=url_link)] for file in files ]
+            btn = [[InlineKeyboardButton(text=f"{file.file_name}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")),
+                    InlineKeyboardButton(text=f"{get_size(file.file_size)}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}"))] for file in files ]
     else:        
         if settings["button"]:
             btn = [[InlineKeyboardButton(text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{nxreq}#{file.file_id}')] for file in files ]
