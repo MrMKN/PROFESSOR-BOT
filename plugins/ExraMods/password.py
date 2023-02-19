9b40afd9ccaa0becf305dc5f10f95d0c9fef524e
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-02-20 00:52:31 +0530

    Update password.py

--- plugins/ExraMods/password.py
+++ plugins/ExraMods/password.py
@@ -7,8 +7,11 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 async def password(bot, update):
     message = await update.reply_text(text="`Processing...`")
     password = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+".lower()
-    ST = ["5", "7", "6", "9", "10", "12", "14", "8", "13"]
-    qw = random.choice(ST)
+    if len(update.command) > 1:
+        qw = update.text.split(" ", 1)[1]
+    else:
+        ST = ["5", "7", "6", "9", "10", "12", "14", "8", "13"] 
+        qw = random.choice(ST)
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
