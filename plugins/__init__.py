4e54e30cb82f2766ed9dbdb47e4eeb9462a3bf8e
Dhanushfan1 <93853054+Dhanushfan1@users.noreply.github.com>
2023-02-19 15:11:52 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -4,7 +4,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Mᴋɴ Bᴏᴛᴢ")
+    return web.json_response("GKP Bᴏᴛᴢ")
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
