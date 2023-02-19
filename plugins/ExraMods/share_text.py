36edf9f89bd5003e8a9092637dfb4eb476e4788d
Dhanushfan1 <93853054+Dhanushfan1@users.noreply.github.com>
2023-02-19 15:07:44 +0530

    Update share_text.py

--- plugins/ExraMods/share_text.py
+++ plugins/ExraMods/share_text.py
@@ -16,7 +16,7 @@ async def share_text(client, message):
         await message.reply_text(
             text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",                
             reply_to_message_id=reply_id,               
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Support Chat", url=f"https://t.me/MKN_BOTZ_DISCUSSION_GROUP")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Support Chat", url=f"https://t.me/GKP_Movie_Corner")]])
             )                                                   
         return
     await message.reply_text(
