45d37a430d75033b252d21161791a8090e5b1a6c
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-02-20 21:02:16 +0530

    Update query.py
    Fix spac issue with use strip()

--- plugins/query.py
+++ plugins/query.py
@@ -265,7 +265,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         
     if query.data.startswith("file"):        
         ident, req, file_id = query.data.split("#")
-        if BUTTON_LOCK.lower() in ["true", "yes", "1", "enable", "y"]:
+        if BUTTON_LOCK.strip().lower() in ["true", "yes", "1", "enable", "y"]:
             if int(req) not in [query.from_user.id, 0]:
                 return await query.answer(BUTTON_LOCK_TEXT.format(query=query.from_user.first_name), show_alert=True)             
         files_ = await get_file_details(file_id)
