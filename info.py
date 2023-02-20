b4e5e0188d9161f582812e35b7bc41019b7be974
BrokenBoy204 <111448245+Brokenboy204@users.noreply.github.com>
2023-02-20 10:56:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ BOT_TOKEN = environ('BOT_TOKEN','5968791476:AAEZdQQcVuJHHwhPgFO-NJV4p9eLpE4KcIc'
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
+PICS = (environ.get('PICS' ,'https://graph.org/file/9bff9276c4e5329816aa0.jpg https://graph.org/file/77ed770fdee5aa484a5a1.jpg https://graph.org/file/e35fa54336bffc6e09c85.jpg')).split()
 BOT_START_TIME = time()
 
 # Admins, Channels & Users
