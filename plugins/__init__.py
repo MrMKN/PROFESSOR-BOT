0230d9a1379f5ccacda94794b1a073c3b7b18468
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-02-22 23:29:41 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -10,3 +10,8 @@ async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
+
+
+__repo__ = "https://github.com/MrMKN/PROFESSOR-BOT"
+__license__ = "GNU GENERAL PUBLIC LICENSE V2"
+__copyright__ = "Copyright (C) 2023-present MrMKN <https://github.com/MrMKN>"
