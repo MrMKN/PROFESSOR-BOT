29cf795adf11cd64fa56d7e2415d923599fdf6fc
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-04-05 15:25:57 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -108,6 +108,20 @@ async def get_search_results(query, file_type=None, max_results=(MAX_RIST_BTNS),
     return files, next_offset, total_results, all_files
 
 
+async def get_all_files(query):
+    query = query.strip()    
+    if not query: raw_pattern = '.'
+    elif ' ' not in query: raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
+    else: raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')   
+    try: regex = re.compile(raw_pattern, flags=re.IGNORECASE)
+    except: return []
+    filter = {'file_name': regex}
+    total_results = await Media.count_documents(filter)    
+    cursor = Media.find(filter)   
+    cursor.sort('$natural', -1)    
+    files = await cursor.to_list(length=total_results) 
+    return files
+
 
 async def get_file_details(query):
     filter = {'file_id': query}
