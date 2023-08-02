755022e467f3569bf16250e76a922b08ec5f790a
divytech <95929956+divytech@users.noreply.github.com>
2023-08-02 17:15:45 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -161,7 +161,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('This Movie Not Found In DataBase')
+            k = await query.message.edit('This Movie Not Found In DataBase 😒😒. DROP YOUR REQUEST WITH @admin TAG.')
             await asyncio.sleep(10)
             await k.delete()
 
