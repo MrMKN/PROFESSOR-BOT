75f129d4d588e32ba509b7d3eb18af19b693a5d0
Radhe Radhe <155654271+radheradhe0010@users.noreply.github.com>
2024-01-15 01:15:48 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -429,13 +429,17 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "start":                        
         buttons = [[
-            InlineKeyboardButton("➕️ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Cʜᴀᴛ ➕", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+            InlineKeyboardButton("🌐 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Cʜᴀᴛ 🌐", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
             InlineKeyboardButton("Sᴇᴀʀᴄʜ 🔎", switch_inline_query_current_chat=''), 
-            InlineKeyboardButton("Cʜᴀɴɴᴇʟ 🔈", url="https://t.me/mkn_bots_updates")
+            InlineKeyboardButton("Cʜᴀɴɴᴇʟ 🔈", url="https://t.me/onlyhdmoviesre")
             ],[      
-            InlineKeyboardButton("Hᴇʟᴩ 🕸️", callback_data="help"),
-            InlineKeyboardButton("Aʙᴏᴜᴛ ✨", callback_data="about")
+            InlineKeyboardButton("🙆 Hᴇʟᴩ 🦾", callback_data="help"),
+            InlineKeyboardButton("💓 Aʙᴏᴜᴛ 💓", callback_data="about")
+        ],[      
+            InlineKeyboardButton("Bollywood 📽️", url="https://t.me/radheradhe765"),
+            InlineKeyboardButton("Hollywood 🍿", url="https://t.me/radheradhe765"),
+            InlineKeyboardButton("Tollywood 🎬", url"https://t.me/radheradhe765")
         ]]
         await query.edit_message_media(InputMediaPhoto(random.choice(PICS), START_MESSAGE.format(user=query.from_user.mention, bot=client.mention), enums.ParseMode.HTML), reply_markup=InlineKeyboardMarkup(buttons))
        
