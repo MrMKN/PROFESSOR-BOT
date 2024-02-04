c9ea7d12dcd81f068313dc428689fb14efd87a6d
Dᴀʀᴋ_Aɴɢᴇʟ <84849690+COLD-ONEZ@users.noreply.github.com>
2024-02-04 08:19:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,6 +15,8 @@ API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 
+SPELL_CHECK_REPLY = is_enabled(environ.get('CHECK_SPELL', 'താങ്കൾ ആവശ്യപ്പെട്ട സീരീസ് ലഭ്യമാവുകയാണെങ്കിൽ ഞങ്ങൾ അപ്‌ലോഡ് ചെയ്യുന്നതായിരിക്കും ദയവായി ഞങ്ങൾ അപ്‌ലോഡ് ചെയ്യുന്നത് വരെ കാത്തിരിക്കുക അപ്‌ലോഡ് ചെയ്ത ശേഷം ഞങ്ങൾ അറിയിക്കുന്നതാണ്.')
+
 # Bot settings
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
 PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
@@ -67,7 +69,6 @@ CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "{file_name}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", None)
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE)
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
-SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "False"), False)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
 MELCOW_NEW_USERS = is_enabled(environ.get('MELCOW_NEW_USERS', "False"), False)
