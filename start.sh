84736d4ff70425c69f879dc35808914c9011d947
Itz Cp <121564783+Cpmodzyt@users.noreply.github.com>
2024-05-07 17:00:42 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Cpmodzyt/PROFESSOR-BOT.git /PROFESSOR-BOT
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /PROFESSOR-BOT
+fi
+cd /PROFESSOR-BOT
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
