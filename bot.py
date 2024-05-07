0c76c091740d09f27fa4b730a450cb9830188a00
Itz Cp <121564783+Cpmodzyt@users.noreply.github.com>
2024-05-07 17:14:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,87 +1,97 @@
-import os, math, logging, datetime, pytz
+
+import sys
+import glob
+import importlib
+from pathlib import Path
+from pyrogram import idle
+import logging
 import logging.config
 
-from pyrogram.errors import BadRequest, Unauthorized
-from pyrogram import Client
-from pyrogram import types
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("cinemagoer").setLevel(logging.ERROR)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("aiohttp").setLevel(logging.ERROR)
+logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
+
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
-from info import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, UPTIME, WEBHOOK, LOG_MSG
-from utils import temp, __repo__, __license__, __copyright__, __version__
+from info import *
+from utils import temp
 from typing import Union, Optional, AsyncGenerator
-
-from plugins import web_server 
+from pyrogram import types
+from Script import script 
+from datetime import date, datetime 
+import pytz
 from aiohttp import web
+from plugins import web_server
 
-# Get logging configurations
-logging.config.fileConfig("logging.conf")
-logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("cinemagoer").setLevel(logging.ERROR)
-logger = logging.getLogger(__name__)
-
-
-class Bot(Client):
-
-    def __init__(self):
-        super().__init__(
-            name="Professor-Bot",
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            workers=10,
-            plugins={"root": "plugins"},
-            sleep_threshold=10,
-        )
-
-    async def start(self):
-        b_users, b_chats = await db.get_banned()
-        temp.BANNED_USERS = b_users
-        temp.BANNED_CHATS = b_chats        
-        await super().start()
-        await Media.ensure_indexes()
-        me = await self.get_me()
-        temp.U_NAME = me.username
-        temp.B_NAME = me.first_name
-        self.id = me.id
-        self.name = me.first_name
-        self.mention = me.mention
-        self.username = me.username
-        self.log_channel = LOG_CHANNEL
-        self.uptime = UPTIME
-        curr = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
-        date = curr.strftime('%d %B, %Y')
-        tame = curr.strftime('%I:%M:%S %p')
-        logger.info(LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__))
-        try: await self.send_message(LOG_CHANNEL, text=LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__), disable_web_page_preview=True)   
-        except Exception as e: logger.warning(f"Bot Isn't Able To Send Message To LOG_CHANNEL \n{e}")
-        if WEBHOOK is True:
-            app = web.AppRunner(await web_server())
-            await app.setup()
-            await web.TCPSite(app, "0.0.0.0").start()
-            logger.info("Web Response Is Running......🕸️")
-            
-    async def stop(self, *args):
-        await super().stop()
-        me = await self.get_me()
-        logger.info(f"{me.first_name} is_...  ♻️Restarting...")
-
-    async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:                       
-        current = offset
-        while True:
-            new_diff = min(200, limit - current)
-            if new_diff <= 0:
-                return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
-            for message in messages:
-                yield message
-                current += 1
+import asyncio
+from pyrogram import idle
+from lazybot import LazyPrincessBot
+from util.keepalive import ping_server
+from lazybot.clients import initialize_clients
 
 
-        
-Bot().run()
+ppath = "plugins/*.py"
+files = glob.glob(ppath)
+LazyPrincessBot.start()
+loop = asyncio.get_event_loop()
 
 
+async def Lazy_start():
+    print('\n')
+    print('Initalizing The Movie Provider Bot')
+    bot_info = await LazyPrincessBot.get_me()
+    LazyPrincessBot.username = bot_info.username
+    await initialize_clients()
+    for name in files:
+        with open(name) as a:
+            patt = Path(a.name)
+            plugin_name = patt.stem.replace(".py", "")
+            plugins_dir = Path(f"plugins/{plugin_name}.py")
+            import_path = "plugins.{}".format(plugin_name)
+            spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
+            load = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(load)
+            sys.modules["plugins." + plugin_name] = load
+            print("The Movie Provider Imported => " + plugin_name)
+    if ON_HEROKU:
+        asyncio.create_task(ping_server())
+    b_users, b_chats = await db.get_banned()
+    temp.BANNED_USERS = b_users
+    temp.BANNED_CHATS = b_chats
+    await Media.ensure_indexes()
+    me = await LazyPrincessBot.get_me()
+    temp.ME = me.id
+    temp.U_NAME = me.username
+    temp.B_NAME = me.first_name
+    LazyPrincessBot.username = '@' + me.username
+    logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+    logging.info(LOG_STR)
+    logging.info(script.LOGO)
+    tz = pytz.timezone('Asia/Kolkata')
+    today = date.today()
+    now = datetime.now(tz)
+    time = now.strftime("%H:%M:%S %p")
+    await send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+    app = web.AppRunner(await web_server())
+    await app.setup()
+    bind_address = "0.0.0.0"
+    await web.TCPSite(app, bind_address, PORT).start()
+    await idle()
 
 
-
+if __name__ == '__main__':
+    try:
+        loop.run_until_complete(Lazy_start())
+    except KeyboardInterrupt:
+        logging.info('Service Stopped Bye 👋')
