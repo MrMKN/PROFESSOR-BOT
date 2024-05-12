915fcce95c47cea31c1992c7d00ce250475530ab
Itz Cp <121564783+Cpmodzyt@users.noreply.github.com>
2024-05-12 06:12:02 +0530

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+app.use(express.static(__dirname + '/'));
+app.get('*', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './src/index.html'));
+});
+app.listen(process.env.PORT || 8080);
