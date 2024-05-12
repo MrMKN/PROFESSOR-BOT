2f997c005e179d04a753d504492049c07f56288c
Itz Cp <121564783+Cpmodzyt@users.noreply.github.com>
2024-05-12 06:25:20 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+{
+    "name": "app-name",
+    "description": "some desc",
+    "version": "0.0.1",
+    "private": true,
+    "engines": {
+        "node": "0.8.12",
+        "npm":  "1.1.49"
+    },
+    "dependencies": {
+        "express" : "2.5.9",
+        "mongoose" : ">=2.6.0",
+        "colibri" : "*",
+        "jade": ">= 0.0.1"
+    }
+      }
