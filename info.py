4a47a040feeaa3f9af567b4de2660756adec8f04
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-06-08 14:12:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,6 +75,14 @@ PROTECT_CONTENT = is_enabled(environ.get('PROTECT_CONTENT', "False"), False)
 PUBLIC_FILE_STORE = is_enabled(environ.get('PUBLIC_FILE_STORE', "True"), True)
 LOG_MSG = "{} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ....✨\n\n🗓️ Dᴀᴛᴇ : {}\n⏰ Tɪᴍᴇ : {}\n\n🖥️ Rᴇᴏᴩ: {}\n🉐 Vᴇʀsɪᴏɴ: {}\n🧾 Lɪᴄᴇɴꜱᴇ: {}\n©️ Cᴏᴩʏʀɪɢʜᴛ: {}"
 
+#not Fucking Copy Paste give me credit 
+
+REACTIONS = [
+    "🔥", "❤️", "😍", "⚡", "😇",
+    "🤩", "🎉", "🤗", "🤩", "😎",
+    "🤝", "🤯", "🤓", "😶"
+]
+
 
 
 
