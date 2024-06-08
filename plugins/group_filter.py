53d705e70da59c18cf433608a0dd6483eeb97631
Itz Cp <121564783+Cpmodzyt@users.noreply.github.com>
2024-06-08 16:57:20 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -100,7 +100,7 @@ async def next_page(bot, query):
             btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'files#{nxreq}#{file.file_id}'),
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'files#{nxreq}#{file.file_id}')] for file in files ]
 
-    btn.insert(0, [InlineKeyboardButton("🔗 Updates 🔗", url='https://t.me/FilmZone_Official' )])
+    btn.insert(0, [InlineKeyboardButton("🔗 Uᴘᴅᴀᴛᴇꜱ 🔗", url='https://t.me/FilmZone_Official' )])
     if 0 < offset <= 10:
         off_set = 0
     elif offset == 0:
