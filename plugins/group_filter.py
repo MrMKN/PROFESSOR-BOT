a33c6cd1064bdd60c8a4b56d61714eb31ad7c3dc
Itz Cp <121564783+Cpmodzyt@users.noreply.github.com>
2024-06-09 19:25:07 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -100,7 +100,7 @@ async def next_page(bot, query):
             btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'files#{nxreq}#{file.file_id}'),
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'files#{nxreq}#{file.file_id}')] for file in files ]
 
-    btn.insert(0, [InlineKeyboardButton("🔺 Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ 🔻", url='https://t.me/FilmZone_Official' )])
+    btn.insert(0, [InlineKeyboardButton("▫️ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ▫️", url='https://t.me/FilmZone_Official' )])
     if 0 < offset <= 10:
         off_set = 0
     elif offset == 0:
@@ -222,7 +222,7 @@ async def auto_filter(client, msg, spoll=False):
             btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'{pre}#{req}#{file.file_id}'),
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'{pre}#{req}#{file.file_id}')] for file in files ] 
 
-    btn.insert(0, [InlineKeyboardButton("🔺 Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ 🔻", url='https://t.me/FilmZone_Official' )])
+    btn.insert(0, [InlineKeyboardButton("▫️ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ▫️", url='https://t.me/FilmZone_Official' )])
     if offset != "":
         key = f"{message.chat.id}-{message.id}"
         temp.GP_BUTTONS[key] = search
