a824ea3cd1df9618412c256c21ac8a76dfca23b9
Esrog <98402018+Esrog@users.noreply.github.com>
2024-10-08 12:32:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from database.users_chats_db import db
 from database.ia_filterdb import  Media
 from typing import Union, Optional, AsyncGenerator
 from utils import temp, __repo__, __license__, __copyright__, __version__
-from info import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, UPTIME, WEB_SUPPORT, LOG_MSG
+from info import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, UPTIME, LOG_MSG
 
 # Get logging configurations
 logging.config.fileConfig("logging.conf")
@@ -45,14 +45,16 @@ class Bot(Client):
         tame = curr.strftime('%I:%M:%S %p')
         logging.info(LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__))
         
-        try: await self.send_message(LOG_CHANNEL, text=LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__), disable_web_page_preview=True)   
-        except Exception as e: logging.warning(f"Bot Isn't Able To Send Message To LOG_CHANNEL \n{e}")
+        try:
+            await self.send_message(LOG_CHANNEL, text=LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__), disable_web_page_preview=True)   
+        except Exception as e:
+            logging.warning(f"Bot Isn't Able To Send Message To LOG_CHANNEL \n{e}")
         
-        if bool(WEB_SUPPORT) is True:
-            app = web.AppRunner(web.Application(client_max_size=30000000))
-            await app.setup()
-            await web.TCPSite(app, "0.0.0.0", 8080).start()
-            logging.info("Web Response Is Running......🕸️")
+        # Web server always starts here on port 8080
+        app = web.AppRunner(web.Application(client_max_size=30000000))
+        await app.setup()
+        await web.TCPSite(app, "0.0.0.0", 8080).start()
+        logging.info("Web Response Is Running......🕸️")
             
     async def stop(self, *args):
         await super().stop()
@@ -72,6 +74,7 @@ class Bot(Client):
 
         
 Bot().run()
+
 
 
 
